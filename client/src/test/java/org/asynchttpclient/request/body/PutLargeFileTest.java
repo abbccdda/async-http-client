6deebb3fb75e8bda6ac1161c2408efd624bd3174
cs427fa16staff <bchen11@outlook.com>
2016-11-19 13:06:04 -0600

    add host

--- client/src/test/java/org/asynchttpclient/request/body/PutLargeFileTest.java
+++ client/src/test/java/org/asynchttpclient/request/body/PutLargeFileTest.java
@@ -40,12 +40,12 @@ public class PutLargeFileTest extends AbstractBasicTest {
 
         File file = createTempFile(1024 * 1024);
 
-        int timeout = (int) file.length() / 1000;
+        int timeout = (int) file.length() / 100;
 
-//        try (AsyncHttpClient client = asyncHttpClient(config().setConnectTimeout(timeout))) {
-//            Response response = client.preparePut(getTargetUrl()).setBody(file).execute().get();
-//            assertEquals(response.getStatusCode(), 200);
-//        }
+        try (AsyncHttpClient client = asyncHttpClient(config().setConnectTimeout(timeout))) {
+            Response response = client.preparePut(getTargetUrl()).setBody(file).execute().get();
+            assertEquals(response.getStatusCode(), 200);
+        }
     }
 
     @Test(groups = "standalone")
